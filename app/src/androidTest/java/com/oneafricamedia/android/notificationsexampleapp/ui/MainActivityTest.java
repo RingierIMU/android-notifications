33d7c1642731b51ed62ea71feaf090015a7436b2
Marc Herbrechter <marc.herbrechter@gmail.com>
2016-10-06 18:02:52 +0200

    Adds simple test

---
+++ app/src/androidTest/java/com/oneafricamedia/android/notificationsexampleapp/ui/MainActivityTest.java
@@ -0,0 +1,26 @@
+package com.oneafricamedia.android.notificationsexampleapp.ui;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.pressBack;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        pressBack();
+    }
+
+}
